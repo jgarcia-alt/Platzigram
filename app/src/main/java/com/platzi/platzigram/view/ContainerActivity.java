5bf9d88aaae82600890336f9597dde99528f01e4
Jonatan <jgarcia_alt@outlook.com>
2019-10-10 20:33:35 -0500

    Menude cerrar sesion agregado

--- app/src/main/java/com/platzi/platzigram/view/ContainerActivity.java
+++ app/src/main/java/com/platzi/platzigram/view/ContainerActivity.java
@@ -1,14 +1,24 @@
 package com.platzi.platzigram.view;
 
 import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.platzi.platzigram.R;
+import com.platzi.platzigram.login.view.CreateAccountActivity;
+import com.platzi.platzigram.login.view.LoginActivity;
 import com.platzi.platzigram.post.view.HomeFragment;
 import com.platzi.platzigram.view.fragment.ProfileFragment;
 import com.platzi.platzigram.view.fragment.SearchFragment;
@@ -16,6 +26,10 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
+    private static final String TAG = "ContainerActivity";
+    private FirebaseAuth firebaseAuth;
+
+private FirebaseAuth.AuthStateListener authStateListener;
 
     @SuppressLint("ResourceType")
 
@@ -23,7 +37,7 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-
+        firebaseInitialize();
         final BottomBar bottomBar = findViewById(R.id.bottombar);
         bottomBar.setDefaultTabPosition(1);
                bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
@@ -54,4 +68,44 @@ public class ContainerActivity extends AppCompatActivity {
                    }
                });
         }
+    private void firebaseInitialize(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    Log.w(TAG, "Usuario Logeado" + firebaseUser.getEmail());
+                }else{
+                    Log.w(TAG, "Usuario no logeado");
+                }
+            }
+        };
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mSignOut:
+                firebaseAuth.signOut();
+
+                if (LoginManager.getInstance()!=null){
+                    LoginManager.getInstance().logOut();
+                }
+                Toast.makeText(this, "Se cerro la sesion", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(ContainerActivity.this, LoginActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.mAbout:
+                Toast.makeText(this, "Platzi", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
